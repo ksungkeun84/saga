3eb9b9e5da36181cf92a4fe3074f97dbb192c93e
Anshuman <ans.suri@northeastern.edu>
2025-03-28 15:44:19 -0400

    Add seed email/calendar data for 2 agents

--- tools/email.py
+++ tools/email.py
@@ -1,14 +1,30 @@
 from pymongo import MongoClient
 from datetime import datetime
+from typing import List
 
 from tools.base import BaseTool
 
 
 class LocalEmailClientTool(BaseTool):
-    def __init__(self, user_email: str):
+    def __init__(self, user_name: str, user_email: str):
         super().__init__("email")
         self.client = MongoClient(self.mongo_uri)
         self.user_email = user_email
+        self.user_name = user_name
+    
+    def seed_data(self, data: List[dict]):
+        db = self.client.get_database(self.tool_name)
+        collection_self = db.get_collection(self.user_email + "_sent")
+
+        for email in data: 
+            # Insert into self sent collection
+            collection_self.insert_one(email)
+
+            # format is "name <email>" - we want email out of it
+            recipient = self._get_email_from_field(email["to"])
+            collection_recipient = db.get_collection(recipient + "_inbox")
+            # Insert into recipient inbox collection
+            collection_recipient.insert_one(email)
     
     def get_emails(self, limit: int = 10):
         """
@@ -24,9 +40,11 @@ class LocalEmailClientTool(BaseTool):
 
         # Convert to list of dictionaries
         emails = list(emails)
-        # Remove objectid : we only need to/from/subject/body/time
+        # Remove objectid : we only need from/subject/body/time
         for email in emails:
             email.pop("_id", None)
+            email.pop("to", None)
+
         return emails
 
     def search_emails_by_query(self, query: str):
@@ -40,9 +58,10 @@ class LocalEmailClientTool(BaseTool):
         emails = collection.find({"$text": {"$search": query}})
         # Convert to list of dictionaries
         emails = list(emails)
-        # Remove objectid : we only need to/from/subject/body/time
+        # Remove objectid : we only need from/subject/body/time
         for email in emails:
             email.pop("_id", None)
+            email.pop("to", None)
         return emails
 
     def send_email(self, to: str, subject: str, body: str):
@@ -59,7 +78,7 @@ class LocalEmailClientTool(BaseTool):
         collection_recipient = db.get_collection(to + "_inbox")
 
         email = {
-            "from": self.user_email,
+            "from": f"{self.user_name} <{self.user_email}>",
             "to": to,
             "subject": subject,
             "body": body,
